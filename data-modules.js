6b2a1c4e1033a6b6b3afbe5dc7b8ea3e119184cc
abdelhamidbakhta <abdelhamidbakhta@users.noreply.github.com>
2023-11-03 10:12:44 +0000

    deploy: 6f683b4c98c75b5cf4ce36292f12ac3457619968

--- data-modules.js
+++ data-modules.js
@@ -1,1 +1,1 @@
-var modules =[{"name":"lib","file":0,"main":66,"table":{"builtin":2,"lib":0,"std":1}},{"name":"std","file":0,"main":69,"table":{}},{"name":"builtin","file":0,"main":31849,"table":{}}];
+var modules =[{"name":"lib","file":0,"main":66,"table":{"builtin":2,"std":1,"lib":0}},{"name":"std","file":0,"main":69,"table":{}},{"name":"builtin","file":0,"main":31849,"table":{}}];
