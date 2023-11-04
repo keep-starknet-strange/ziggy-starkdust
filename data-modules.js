d19628c6990289e0514c198922e63841f79b8609
lana-shanghai <lana-shanghai@users.noreply.github.com>
2023-11-04 23:15:39 +0000

    deploy: 6c0167b3c8711a5c79fae49f74c161a288531885

--- data-modules.js
+++ data-modules.js
@@ -1,1 +1,1 @@
-var modules =[{"name":"lib","file":0,"main":66,"table":{"builtin":2,"std":1,"lib":0}},{"name":"std","file":0,"main":69,"table":{}},{"name":"builtin","file":0,"main":31849,"table":{}}];
+var modules =[{"name":"lib","file":0,"main":66,"table":{"std":1,"lib":0,"builtin":2}},{"name":"std","file":0,"main":69,"table":{}},{"name":"builtin","file":0,"main":31849,"table":{}}];
