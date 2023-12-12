3b4b226e0f14215a662f106500786ac5dd3a4762
lambda-0x <0xlambda@protonmail.com>
2023-12-12 20:10:44 +0530

    refactor: encapsulate setUpMemory method in Memory structure

--- src/vm/core_test.zig
+++ src/vm/core_test.zig
@@ -65,8 +65,7 @@ test "CairoVM: deduceMemoryCell builtin valid" {
         true,
     ) });
 
-    try memory.setUpMemory(
-        vm.segments.memory,
+    try vm.segments.memory.setUpMemory(
         std.testing.allocator,
         .{
             .{ .{ 0, 5 }, .{10} },
@@ -947,8 +946,7 @@ test "set get value in vm memory" {
     const address = Relocatable.init(1, 0);
     const value = MaybeRelocatable.fromFelt(starknet_felt.Felt252.fromInteger(42));
 
-    try memory.setUpMemory(
-        vm.segments.memory,
+    try vm.segments.memory.setUpMemory(
         std.testing.allocator,
         .{
             .{ .{ 1, 0 }, .{42} },
@@ -1183,8 +1181,7 @@ test "compute operands add AP" {
     const op1_addr = Relocatable.init(1, 2);
     const op1_val = MaybeRelocatable{ .felt = Felt252.fromInteger(3) };
 
-    try memory.setUpMemory(
-        vm.segments.memory,
+    try vm.segments.memory.setUpMemory(
         std.testing.allocator,
         .{
             .{ .{ 1, 0 }, .{5} },
@@ -1242,8 +1239,7 @@ test "compute operands mul FP" {
 
     const op1_addr = Relocatable.init(1, 2);
     const op1_val = MaybeRelocatable{ .felt = Felt252.fromInteger(3) };
-    try memory.setUpMemory(
-        vm.segments.memory,
+    try vm.segments.memory.setUpMemory(
         std.testing.allocator,
         .{
             .{ .{ 1, 0 }, .{6} },
@@ -1454,8 +1450,7 @@ test "CairoVM: computeOp0Deductions with a valid built in and non null deduceMem
         &instance_def,
         true,
     ) });
-    try memory.setUpMemory(
-        vm.segments.memory,
+    try vm.segments.memory.setUpMemory(
         std.testing.allocator,
         .{
             .{ .{ 0, 5 }, .{10} },
@@ -1505,8 +1500,7 @@ test "CairoVM: computeSegmentsEffectiveSizes should return the computed effectiv
     var vm = try CairoVM.init(std.testing.allocator, .{});
     defer vm.deinit();
 
-    try memory.setUpMemory(
-        vm.segments.memory,
+    try vm.segments.memory.setUpMemory(
         std.testing.allocator,
         .{
             .{ .{ 0, 0 }, .{1} },
@@ -1630,8 +1624,7 @@ test "CairoVM: getRelocatable with value should return a MaybeRelocatable" {
     var vm = try CairoVM.init(std.testing.allocator, .{});
     defer vm.deinit();
 
-    try memory.setUpMemory(
-        vm.segments.memory,
+    try vm.segments.memory.setUpMemory(
         std.testing.allocator,
         .{
             .{ .{ 34, 12 }, .{5} },
@@ -1733,8 +1726,7 @@ test "CairoVM: getFelt should return Felt252 if available at the given address" 
     );
     defer vm.deinit();
 
-    try memory.setUpMemory(
-        vm.segments.memory,
+    try vm.segments.memory.setUpMemory(
         std.testing.allocator,
         .{
             .{ .{ 10, 30 }, .{23} },
@@ -1757,8 +1749,7 @@ test "CairoVM: getFelt should return ExpectedInteger error if Relocatable instea
     );
     defer vm.deinit();
 
-    try memory.setUpMemory(
-        vm.segments.memory,
+    try vm.segments.memory.setUpMemory(
         std.testing.allocator,
         .{
             .{ .{ 10, 30 }, .{ 3, 7 } },
@@ -1783,8 +1774,7 @@ test "CairoVM: computeOp1Deductions should return op1 from deduceMemoryCell if n
         &instance_def,
         true,
     ) });
-    try memory.setUpMemory(
-        vm.segments.memory,
+    try vm.segments.memory.setUpMemory(
         std.testing.allocator,
         .{
             .{ .{ 0, 5 }, .{10} },
@@ -1963,8 +1953,7 @@ test "CairoVM: InserDeducedOperands should insert operands if set as deduced" {
 
     const op1_addr = Relocatable.init(1, 2);
     const op1_val = MaybeRelocatable{ .felt = Felt252.fromInteger(3) };
-    try memory.setUpMemory(
-        vm.segments.memory,
+    try vm.segments.memory.setUpMemory(
         std.testing.allocator,
         .{},
     );
@@ -2017,8 +2006,7 @@ test "CairoVM: InserDeducedOperands insert operands should not be inserted if no
 
     const op1_addr = Relocatable.init(1, 2);
     const op1_val = MaybeRelocatable{ .felt = Felt252.fromInteger(3) };
-    try memory.setUpMemory(
-        vm.segments.memory,
+    try vm.segments.memory.setUpMemory(
         std.testing.allocator,
         .{},
     );
