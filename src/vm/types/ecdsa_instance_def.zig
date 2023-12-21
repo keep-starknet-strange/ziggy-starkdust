66fafea60170696d1d8873099a8dfa38f9499803
Patrice Tisserand <p.tisserand@gmail.com>
2023-12-21 01:05:57 +0100

    ecdsa instance builtin

--- src/vm/types/ecdsa_instance_def.zig
+++ src/vm/types/ecdsa_instance_def.zig
@@ -1,3 +1,9 @@
+const std = @import("std");
+const expectEqual = std.testing.expectEqual;
+
+pub const CELLS_PER_SIGNATURE: u32 = 2;
+pub const _INPUTCELLS_PER_SIGNATURE: u32 = 2;
+
 /// Represents a ECDSA Instance Definition.
 pub const EcdsaInstanceDef = struct {
     const Self = @This();
@@ -28,4 +34,54 @@ pub const EcdsaInstanceDef = struct {
             .n_hash_bits = 251,
         };
     }
+
+    pub fn _cells_per_builtin(_: Self) u32 {
+        return CELLS_PER_SIGNATURE;
+    }
+
+    pub fn _range_check_units_per_builtin(_: *const Self) u32 {
+        return 0;
+    }
 };
+
+test "EcdsaInstanceDef: test from" {
+    const instance = EcdsaInstanceDef{
+        .ratio = 8,
+        .repetitions = 1,
+        .height = 256,
+        .n_hash_bits = 251,
+    };
+    try expectEqual(
+        EcdsaInstanceDef.from(8),
+        instance,
+    );
+}
+
+test "EcdsaInstanceDef: test init" {
+    const instance = EcdsaInstanceDef{
+        .ratio = 512,
+        .repetitions = 1,
+        .height = 256,
+        .n_hash_bits = 251,
+    };
+    try expectEqual(
+        EcdsaInstanceDef.init(),
+        instance,
+    );
+}
+
+test "EcdsaInstanceDef: test cells_per_builtin" {
+    const instance = EcdsaInstanceDef.init();
+    try expectEqual(
+        instance._cells_per_builtin(),
+        CELLS_PER_SIGNATURE,
+    );
+}
+
+test "EcdsaInstanceDef: test range_check_units_per_builtin" {
+    const instance = EcdsaInstanceDef.init();
+    try expectEqual(
+        instance._range_check_units_per_builtin(),
+        0,
+    );
+}
