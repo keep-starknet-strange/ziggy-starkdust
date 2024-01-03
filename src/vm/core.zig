56a95661e7798291f84a981d1ddd3afdc2e44440
Dhruv Kelawala <dhruvrk2000@gmail.com>
2024-01-03 15:45:55 +0000

    feat: implement verifyAutoDeductionsForAddr (#290)
    * feat: implement verifyAutoDeductionsForAddr
    
    * chore: enhance tests
    
    * chore: add docs

--- src/vm/core.zig
+++ src/vm/core.zig
@@ -493,6 +493,28 @@ pub const CairoVM = struct {
         }
     }
 
+    /// Verifies the auto deductions for a given memory address.
+    ///
+    /// This function checks if the value deduced by the builtin matches the current value
+    /// at the given address in the VM's memory. If they do not match, it returns an error
+    /// indicating an inconsistent auto deduction.
+    ///
+    /// ## Arguments
+    /// - `allocator`: The allocator instance to use for memory operations.
+    /// - `addr`: The memory address to verify.
+    /// - `builtin`: The BuiltinRunner instance used for deducing the memory cell.
+    ///
+    /// ## Returns
+    /// - `void`: Returns nothing on success.
+    /// - `CairoVMError.InconsistentAutoDeduction`: Returns an error if the deduced value does not match the memory.
+    pub fn verifyAutoDeductionsForAddr(self: *const Self, allocator: Allocator, addr: Relocatable, builtin: *const BuiltinRunner) !void {
+        const value = try builtin.deduceMemoryCell(allocator, addr, self.segments.memory) orelse return;
+        const current_value = self.segments.memory.get(addr) orelse return;
+        if (!value.eq(current_value)) {
+            return CairoVMError.InconsistentAutoDeduction;
+        }
+    }
+
     /// Attempts to deduce `op0` and `res` for an instruction, given `dst` and `op1`.
     ///
     /// # Arguments
