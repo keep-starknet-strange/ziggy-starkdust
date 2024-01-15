7ee2c89b0646eb70c2c3fa80e721a4f9cbb2252b
Timothy Edison <timothyedison@hotmail.com>
2024-01-15 18:14:25 +0000

    Remove unnecessary mut var from hash builtin_runner

--- src/vm/builtins/builtin_runner/builtin_runner.zig
+++ src/vm/builtins/builtin_runner/builtin_runner.zig
@@ -123,8 +123,7 @@ pub const BuiltinRunner = union(enum) {
             .Bitwise => |bitwise| try bitwise.deduceMemoryCell(address, memory),
             .EcOp => |ec| ec.deduceMemoryCell(address, memory),
             .Hash => |*hash| {
-                var mut_hash = hash;
-                return mut_hash.deduceMemoryCell(address, memory);
+                return hash.deduceMemoryCell(address, memory);
             },
             .Output => |output| output.deduceMemoryCell(address, memory),
             .RangeCheck => |range_check| range_check.deduceMemoryCell(address, memory),
