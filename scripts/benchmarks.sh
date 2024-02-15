5bcab1e5fbc351d4762843d60588257fd010a27e
aniket-prajapati <anikethprajapati@gmail.com>
2024-02-15 22:01:12 +0530

    updated benchmark script formatting issue

--- scripts/benchmarks.sh
+++ scripts/benchmarks.sh
@@ -12,5 +12,6 @@ for file in $(ls ${BENCH_DIR} | grep .cairo | sed -E 's/\.cairo//'); do
     cairo-compile --cairo_path="${BENCH_DIR}" ${BENCH_DIR}/${file}.cairo --output ${BENCH_DIR}/${file}.json --proof_mode
     echo "Running ${file} benchmark"
     hyperfine --show-output \
-        -n "cairo-vm (Rust)" "${CAIRO_VM_CLI} ${BENCH_DIR}/${file}.json --proof_mode --memory_file /dev/null --trace_file /dev/null --layout "all_cairo""
+        -n "cairo-vm (Rust)" "cairo-vm-cli ${BENCH_DIR}/${file}.json --proof_mode --memory_file /dev/null --trace_file /dev/null --layout all_cairo" \
+        -n "cairo-vm (Zig)" "ziggy-starkdust execute --filename ${BENCH_DIR}/${file}.json --enable-trace=true --output-memory=/dev/null --output-trace=/dev/null --layout all_cairo"
 done
