4af05a2710c9c2046eeabe89908b1937734cfd3a
aniket-prajapati <anikethprajapati@gmail.com>
2024-02-15 22:45:32 +0530

    updated local variables in benchmarking script

--- scripts/benchmarks.sh
+++ scripts/benchmarks.sh
@@ -3,15 +3,15 @@
 set -e
 source ../cairo-vm-env/bin/activate
 
-CAIRO_VM_CLI=../cairo-vm/target/release/cairo-vm-cli
 BENCH_DIR=../cairo_programs/benchmarks
-export PATH=$PATH:$(pwd)/../zig-out/bin:$(pwd)/../cairo-vm/target/release/
+CAIRO_VM_CLI=../cairo-vm/target/release/cairo-vm-cli
+ZIG_CLI=../zig-out/bin/ziggy-starkdust
 
 for file in $(ls ${BENCH_DIR} | grep .cairo | sed -E 's/\.cairo//'); do
     echo "Compiling ${file} program..."
     cairo-compile --cairo_path="${BENCH_DIR}" ${BENCH_DIR}/${file}.cairo --output ${BENCH_DIR}/${file}.json --proof_mode
     echo "Running ${file} benchmark"
     hyperfine --show-output \
-        -n "cairo-vm (Rust)" "cairo-vm-cli ${BENCH_DIR}/${file}.json --proof_mode --memory_file /dev/null --trace_file /dev/null --layout all_cairo" \
-        -n "cairo-vm (Zig)" "ziggy-starkdust execute --filename ${BENCH_DIR}/${file}.json --enable-trace=true --output-memory=/dev/null --output-trace=/dev/null --layout all_cairo"
+        -n "cairo-vm (Rust)" "${CAIRO_VM_CLI} ${BENCH_DIR}/${file}.json --proof_mode --memory_file /dev/null --trace_file /dev/null --layout all_cairo" \
+        -n "cairo-vm (Zig)" "${ZIG_CLI} execute --filename ${BENCH_DIR}/${file}.json --enable-trace=true --output-memory=/dev/null --output-trace=/dev/null --layout all_cairo"
 done
