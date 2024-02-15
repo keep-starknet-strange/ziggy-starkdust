76a62cec4055092c0669c5a38cdbe2477e366caa
aniket-prajapati <anikethprajapati@gmail.com>
2024-02-15 22:00:05 +0530

    updated benchmark script formatting issue

--- scripts/benchmarks.sh
+++ scripts/benchmarks.sh
@@ -3,6 +3,7 @@
 set -e
 source ../cairo-vm-env/bin/activate
 
+CAIRO_VM_CLI=../cairo-vm/target/release/cairo-vm-cli
 BENCH_DIR=../cairo_programs/benchmarks
 export PATH=$PATH:$(pwd)/../zig-out/bin:$(pwd)/../cairo-vm/target/release/
 
@@ -11,6 +12,5 @@ for file in $(ls ${BENCH_DIR} | grep .cairo | sed -E 's/\.cairo//'); do
     cairo-compile --cairo_path="${BENCH_DIR}" ${BENCH_DIR}/${file}.cairo --output ${BENCH_DIR}/${file}.json --proof_mode
     echo "Running ${file} benchmark"
     hyperfine --show-output \
-        -n "cairo-vm (Rust)" "cairo-vm-cli ${BENCH_DIR}/${file}.json --proof_mode --memory_file /dev/null --trace_file /dev/null --layout all_cairo" \
-        -n "cairo-vm (Zig)" "ziggy-starkdust execute --filename ${BENCH_DIR}/${file}.json --enable-trace=true --output-memory=/dev/null --output-trace=/dev/null --layout all_cairo"
+        -n "cairo-vm (Rust)" "${CAIRO_VM_CLI} ${BENCH_DIR}/${file}.json --proof_mode --memory_file /dev/null --trace_file /dev/null --layout "all_cairo""
 done
