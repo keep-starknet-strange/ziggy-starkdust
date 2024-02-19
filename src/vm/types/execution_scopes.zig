64009312dc95cb8d31a8afbf373d3d0f1f9dfa7c
Nikita Orlov <stringnickq@gmail.com>
2024-02-19 15:12:56 +0100

    feat/math-hint (#381)
    * wip: hint utils and assert_nn math hint
    
    * fix std options struct
    
    * math hint some code
    
    * math hints with tools
    
    * fix review and additional math hint
    
    * fix review
    
    * fix hint compile and get hint data
    
    * additional hints and test for signed methods
    
    * additional math hints
    
    ---------
    
    Co-authored-by: Lana Ivina <svetlana.konstantinovna@gmail.com>
    Co-authored-by: lanaivina <31368580+lana-shanghai@users.noreply.github.com>

--- src/vm/types/execution_scopes.zig
+++ src/vm/types/execution_scopes.zig
@@ -1,16 +1,36 @@
 const std = @import("std");
 const ArrayList = std.ArrayList;
 const Allocator = std.mem.Allocator;
+const Felt252 = @import("../../math/fields/starknet.zig").Felt252;
+const HintError = @import("../error.zig").HintError;
 
 const HintType = union(enum) {
     // TODO: Add missing types
-    Felt252,
-    u64,
+    felt: Felt252,
+    u64: u64,
 };
 
 pub const ExecutionScopes = struct {
     const Self = @This();
     data: ArrayList(std.StringHashMap(HintType)),
+
+    pub fn init(allocator: Allocator) !Self {
+        var d = ArrayList(std.StringHashMap(HintType)).init(allocator);
+        errdefer d.deinit();
+
+        try d.append(std.StringHashMap(HintType).init(allocator));
+
+        return .{
+            .data = d,
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        for (self.data.items) |*it| {
+            it.deinit();
+        }
+        self.data.deinit();
+    }
 
     pub fn enterScope(self: *Self, scope: std.StringHashMap(HintType)) void {
         self.data.append(scope);
@@ -18,5 +38,29 @@ pub const ExecutionScopes = struct {
 
     pub fn exitScope(self: *Self) void {
         self.data.pop();
+    }
+
+    ///Returns the value in the current execution scope that matches the name and is of the given generic type
+    pub fn get(self: *const Self, name: []const u8) !HintType {
+        return if (self.getLocalVariableMut()) |variable| variable.get(name) orelse HintError.VariableNotInScopeError or HintError.VariableNotInScopeError;
+    }
+
+    pub fn getFelt(self: *const Self, name: []const u8) !Felt252 {
+        return switch (try self.get(name)) {
+            .felt => |f| f,
+            else => HintError.VariableNotInScopeError,
+        };
+    }
+
+    ///Returns a dictionary containing the variables present in the current scope
+    pub fn getLocalVariableMut(self: *const Self) ?*std.StringHashMap(HintType) {
+        if (self.data.items.len > 0) return &self.data.items[self.data.items.len - 1];
+
+        return null;
+    }
+
+    ///Creates or updates an existing variable given its name and boxed value
+    pub fn assignOrUpdateVariable(self: *Self, var_name: []const u8, var_value: HintType) !void {
+        if (self.getLocalVariableMut()) |local_variables| try local_variables.put(var_name, var_value);
     }
 };
