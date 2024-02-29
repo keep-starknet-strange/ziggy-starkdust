174d6bf925510be4f66d6efd60fb1b837e65d3fe
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2024-02-29 21:19:55 +0100

    Add garaga hint (#433)
    * Add garaga hint
    
    * rename hint
    
    ---------
    
    Co-authored-by: lanaivina <31368580+lana-shanghai@users.noreply.github.com>

--- src/hint_processor/hint_processor_def.zig
+++ src/hint_processor/hint_processor_def.zig
@@ -19,6 +19,7 @@ const Relocatable = @import("../vm/memory/relocatable.zig").Relocatable;
 const hint_codes = @import("builtin_hint_codes.zig");
 const math_hints = @import("math_hints.zig");
 const memcpy_hint_utils = @import("memcpy_hint_utils.zig");
+const felt_bit_length = @import("felt_bit_length.zig");
 
 const deserialize_utils = @import("../parser/deserialize_utils.zig");
 
@@ -197,6 +198,8 @@ pub const CairoVMHintProcessor = struct {
             try memcpy_hint_utils.exitScope(exec_scopes);
         } else if (std.mem.eql(u8, hint_codes.MEMCPY_ENTER_SCOPE, hint_data.code)) {
             try memcpy_hint_utils.memcpyEnterScope(allocator, vm, exec_scopes, hint_data.ids_data, hint_data.ap_tracking);
+        } else if (std.mem.eql(u8, hint_codes.GET_FELT_BIT_LENGTH, hint_data.code)) {
+            try felt_bit_length.getFeltBitLength(allocator, vm, hint_data.ids_data, hint_data.ap_tracking);
         } else {}
     }
 
--- src/hint_processor/hint_processor_utils.zig
+++ src/hint_processor/hint_processor_utils.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const expectEqual = std.testing.expectEqual;
+const expect = std.testing.expect;
 
 const Register = @import("../vm/instructions.zig").Register;
 const ApTracking = @import("../vm/types/programjson.zig").ApTracking;
@@ -25,21 +27,41 @@ pub fn insertValueFromReference(
     } else return HintError.UnknownIdentifierInternal;
 }
 
-///Returns the Integer value stored in the given ids variable
-/// Returns an internal error, users should map it into a more informative type
+/// Retrieves the integer value stored in the given ids variable.
+///
+/// This function retrieves the integer value stored in the given ids variable indicated by the provided `hint_reference`.
+/// If the value is stored as an immediate, it returns the value directly.
+/// Otherwise, it computes the memory address of the variable and retrieves the integer value from memory.
+///
+/// # Parameters
+/// - `vm`: A pointer to the Cairo virtual machine.
+/// - `hint_reference`: The hint reference indicating the variable.
+/// - `ap_tracking`: The AP tracking data.
+///
+/// # Returns
+/// Returns the integer value stored in the variable indicated by the hint reference.
+/// If the variable is not found or if there's an error retrieving the value, it returns an error of type `HintError`.
 pub fn getIntegerFromReference(
     vm: *CairoVM,
     hint_reference: HintReference,
     ap_tracking: ApTracking,
 ) !Felt252 {
-    // if the reference register is none, this means it is an immediate value and we
-    // should return that value.
+    // If the reference register is none, this means it is an immediate value, and we should return that value.
     switch (hint_reference.offset1) {
         .immediate => |int_1| return int_1,
         else => {},
     }
 
-    return if (computeAddrFromReference(hint_reference, ap_tracking, vm)) |var_addr| vm.segments.memory.getFelt(var_addr) catch HintError.WrongIdentifierTypeInternal else HintError.UnknownIdentifierInternal;
+    // std.debug.print(
+    //     "totototot = {any}\n",
+    //     .{computeAddrFromReference(hint_reference, ap_tracking, vm)},
+    // );
+
+    // Compute the memory address of the variable and retrieve the integer value from memory.
+    return if (computeAddrFromReference(hint_reference, ap_tracking, vm)) |var_addr|
+        vm.segments.memory.getFelt(var_addr) catch HintError.WrongIdentifierTypeInternal
+    else
+        HintError.UnknownIdentifierInternal;
 }
 
 ///Returns the Relocatable value stored in the given ids variable
@@ -85,18 +107,59 @@ pub fn getOffsetValueReference(
         return MaybeRelocatable.fromRelocatable(base_addr.addInt(@as(i64, @intCast(refer[1]))) catch unreachable);
     }
 }
-///Computes the memory address of the ids variable indicated by the HintReference as a [Relocatable]
-pub fn computeAddrFromReference(hint_reference: HintReference, hint_ap_tracking: ApTracking, vm: *CairoVM) ?Relocatable {
+
+/// Computes the memory address indicated by the provided hint reference.
+///
+/// This function takes a hint reference, which is a complex data structure indicating the address
+/// of a variable within the Cairo virtual machine's memory. The function computes this address
+/// by following the instructions encoded in the hint reference.
+///
+/// # Parameters
+/// - `hint_reference`: The hint reference indicating the address.
+/// - `hint_ap_tracking`: The AP tracking data associated with the hint reference.
+/// - `vm`: A pointer to the Cairo virtual machine.
+///
+/// # Returns
+/// Returns the computed memory address as a `Relocatable` if successful, or `null` if the address
+/// could not be computed.
+pub fn computeAddrFromReference(
+    hint_reference: HintReference,
+    hint_ap_tracking: ApTracking,
+    vm: *CairoVM,
+) ?Relocatable {
+    // Extract the first offset value from the hint reference.
     const offset1 = switch (hint_reference.offset1) {
-        .reference => getOffsetValueReference(vm, hint_reference, hint_ap_tracking, hint_reference.offset1).?.intoRelocatable() catch unreachable,
+        .reference => if (getOffsetValueReference(
+            vm,
+            hint_reference,
+            hint_ap_tracking,
+            hint_reference.offset1,
+        )) |v|
+            // Convert the offset value to a relocatable address.
+            v.intoRelocatable() catch return null
+        else
+            return null,
         else => return null,
     };
 
+    // Compute the memory address based on the second offset value or a constant value.
     return switch (hint_reference.offset2) {
-        .reference => offset1.addFelt(getOffsetValueReference(vm, hint_reference, hint_ap_tracking, hint_reference.offset2).?.intoFelt() catch unreachable) catch unreachable,
+        .reference => blk: {
+            // If the second offset is a reference, it must be resolved.
+            const value = getOffsetValueReference(
+                vm,
+                hint_reference,
+                hint_ap_tracking,
+                hint_reference.offset2,
+            ) orelse return null;
 
-        .value => |val| offset1.addInt(val) catch unreachable,
+            // Convert the offset value to an unsigned 64-bit integer.
+            const value_int = value.intoU64() catch return null;
 
+            // Add the offset value to the base address.
+            break :blk offset1.addUint(value_int) catch return null;
+        },
+        .value => |val| offset1.addInt(val) catch null,
         else => null,
     };
 }
@@ -121,4 +184,69 @@ pub fn getMaybeRelocatableFromReference(
             MaybeRelocatable.fromRelocatable(var_addr)
     else
         null;
+}
+
+test "computeAddrFromReference: no register in reference" {
+    // Initialize the Cairo virtual machine.
+    var vm = try CairoVM.init(std.testing.allocator, .{});
+    defer vm.deinit(); // Ensure cleanup.
+
+    // Set up memory segments in the virtual machine.
+    try vm.segments.memory.setUpMemory(
+        std.testing.allocator,
+        .{.{ .{ 1, 0 }, .{ 4, 0 } }},
+    );
+    defer vm.segments.memory.deinitData(std.testing.allocator); // Clean up memory data.
+
+    // Create a hint reference with no register information.
+    var hint_reference = HintReference.init(0, 0, false, false);
+    // Set the immediate offset value to 2.
+    hint_reference.offset1 = .{ .immediate = Felt252.fromInt(u8, 2) };
+
+    // Ensure that the computed address is null, as no register information is provided.
+    try expectEqual(null, computeAddrFromReference(hint_reference, .{}, &vm));
+}
+
+test "computeAddrFromReference: failed to get ids" {
+    // Initialize the Cairo virtual machine.
+    var vm = try CairoVM.init(std.testing.allocator, .{});
+    defer vm.deinit(); // Ensure cleanup.
+
+    // Set up memory segments in the virtual machine.
+    try vm.segments.memory.setUpMemory(
+        std.testing.allocator,
+        .{.{ .{ 1, 0 }, .{4} }},
+    );
+    defer vm.segments.memory.deinitData(std.testing.allocator); // Clean up memory data.
+
+    // Create a hint reference with register information pointing to the frame pointer (FP).
+    var hint_reference = HintReference.init(0, 0, false, false);
+    // Set the reference offset to point to an unknown location relative to the frame pointer.
+    hint_reference.offset1 = .{ .reference = .{ .FP, -1, true } };
+
+    // Ensure that the computed address is null due to failure in retrieving the ids variable.
+    try expectEqual(null, computeAddrFromReference(hint_reference, .{}, &vm));
+}
+
+test "getIntegerFromReference: with immediate value" {
+    // Initialize the Cairo virtual machine.
+    var vm = try CairoVM.init(std.testing.allocator, .{});
+    defer vm.deinit(); // Ensure cleanup.
+
+    // Set up memory segments in the virtual machine.
+    try vm.segments.memory.setUpMemory(
+        std.testing.allocator,
+        .{.{ .{ 1, 0 }, .{0} }},
+    );
+    defer vm.segments.memory.deinitData(std.testing.allocator); // Clean up memory data.
+
+    // Create a hint reference with an immediate value.
+    var hint_reference = HintReference.init(0, 0, false, true);
+    hint_reference.offset1 = .{ .immediate = Felt252.fromInt(u8, 2) };
+
+    // Assert that the integer value retrieved from the reference is equal to the expected value.
+    try expectEqual(
+        Felt252.fromInt(u8, 2),
+        getIntegerFromReference(&vm, hint_reference, .{}),
+    );
 }
