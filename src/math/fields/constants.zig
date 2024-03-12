9d76fdf57a133d7cd30c7c35057089dbe19e159d
Sweta Shaw <swt.shw94@gmail.com>
2024-03-12 23:27:39 +0530

    add BASE constant

--- src/math/fields/constants.zig
+++ src/math/fields/constants.zig
@@ -1,2 +1,6 @@
+const Felt252 = @import("starknet.zig").Felt252;
+
 pub const STARKNET_PRIME = 0x800000000000011000000000000000000000000000000000000000000000001;
 pub const FELT_BYTE_SIZE = 32;
+
+pub const BASE: Felt252 = 2 ** 86;
