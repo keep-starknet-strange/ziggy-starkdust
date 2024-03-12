47ba72e0877bee7190af410ce56fe2ecfcdf39f8
Sweta Shaw <swt.shw94@gmail.com>
2024-03-12 23:28:32 +0530

    use std big int

--- src/hint_processor/uint_utils.zig
+++ src/hint_processor/uint_utils.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Felt252 = @import("../math/fields/starknet.zig").Felt252;
+const Int = @import("std").math.big.int.Managed;
 
 pub fn pack(limbs: []const Felt252, num_bits_shift: usize) u256 {
     var result: u256 = 0;
@@ -9,7 +10,7 @@ pub fn pack(limbs: []const Felt252, num_bits_shift: usize) u256 {
     return result;
 }
 
-pub fn split(comptime N: usize, num: *std.big.Int, num_bits_shift: u32) [N]Felt252 {
+pub fn split(comptime N: usize, num: Int, num_bits_shift: u32) [N]Felt252 {
     var num_copy = num;
     const bitmask = ((1 << num_bits_shift) - 1);
     const result: [N]Felt252 = undefined;
