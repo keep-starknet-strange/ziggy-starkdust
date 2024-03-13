ff221e7360679bcb1972a57a1d5dd9b4a2eb6f5b
Tuan Tran <tropicaldog17@gmail.com>
2024-03-13 23:46:10 +0700

    add pack and more tests

--- src/hint_processor/uint_utils.zig
+++ src/hint_processor/uint_utils.zig
@@ -26,7 +26,18 @@ pub fn split(allocator: std.mem.Allocator, num: Int, comptime N: usize, num_bits
     return result;
 }
 
-test "uint256 split64 with uint utils" {
+pub fn pack(allocator: std.mem.Allocator, comptime N: usize, limbs: [N]Felt252, num_bits_shift: usize) !Int {
+    var result = try Int.init(allocator);
+
+    for (0..N) |i| {
+        var limb_to_uint = try Int.initSet(allocator, limbs[i].toInteger());
+        defer limb_to_uint.deinit();
+        try limb_to_uint.shiftLeft(&limb_to_uint, num_bits_shift * i);
+        try result.add(&result, &limb_to_uint);
+    }
+    return result;
+}
+test "split64 with uint utils" {
     var num = try Int.initSet(testing.allocator, 850981239023189021389081239089023);
     defer num.deinit();
     const limbs = try split(testing.allocator, num, 2, 64);
@@ -34,10 +45,27 @@ test "uint256 split64 with uint utils" {
     try std.testing.expectEqualSlices(Felt252, &[2]Felt252{ Felt252.fromInt(u64, 7249717543555297151), Felt252.fromInt(u64, 46131785404667) }, &limbs);
 }
 
-test "uint256 split64 with big a" {
-    var num = try Int.initSet(testing.allocator, 400066369019890261321163226850167045262);
+test "u384 split128 with uint utils" {
+    var num = try Int.initSet(testing.allocator, 6805647338418769269267492148635364229100);
     defer num.deinit();
     const limbs = try split(testing.allocator, num, 2, 128);
+    try std.testing.expectEqualSlices(Felt252, &[2]Felt252{ Felt252.fromInt(u128, 340282366920938463463374607431768211436), Felt252.fromInt(u128, 19) }, &limbs);
+}
 
-    try std.testing.expectEqualSlices(Felt252, &[2]Felt252{ Felt252.fromInt(u128, 59784002098951797857788619418398833806), Felt252.fromInt(u64, 1) }, &limbs);
+test "pack 64 with uint utils" {
+    const limbs = [2]Felt252{ Felt252.fromInt(u64, 7249717543555297151), Felt252.fromInt(u64, 46131785404667) };
+    var num = try pack(testing.allocator, 2, limbs, 64);
+    defer num.deinit();
+    var expected = try Int.initSet(testing.allocator, 850981239023189021389081239089023);
+    defer expected.deinit();
+    try std.testing.expectEqualSlices(usize, expected.limbs, num.limbs);
+}
+
+test "pack 128 with uint utils" {
+    const limbs = [2]Felt252{ Felt252.fromInt(u128, 59784002098951797857788619418398833806), Felt252.fromInt(u128, 1) };
+    var num = try pack(testing.allocator, 2, limbs, 128);
+    defer num.deinit();
+    var expected = try Int.initSet(testing.allocator, 400066369019890261321163226850167045262);
+    defer expected.deinit();
+    try std.testing.expectEqualSlices(usize, expected.limbs, num.limbs);
 }
