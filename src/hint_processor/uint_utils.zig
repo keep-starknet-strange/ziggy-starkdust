7ef1cfa6a3758b2853ec2f9ccb670d20336f8b69
Sweta Shaw <swt.shw94@gmail.com>
2024-03-15 19:42:48 +0530

    remove duplicate functions

--- src/hint_processor/uint_utils.zig
+++ src/hint_processor/uint_utils.zig
@@ -1,28 +1,6 @@
 const std = @import("std");
 const Felt252 = @import("../math/fields/starknet.zig").Felt252;
 const Int = @import("std").math.big.int.Managed;
-
-pub fn pack(limbs: []const Felt252, num_bits_shift: usize) u256 {
-    var result: u256 = 0;
-    for (0..limbs.len) |i| {
-        result += limbs[i].toInteger() << (i * num_bits_shift);
-    }
-    return result;
-}
-
-pub fn split(comptime N: usize, num: Int, num_bits_shift: u32) [N]Felt252 {
-    var num_copy = num;
-    const bitmask = ((1 << num_bits_shift) - 1);
-    const result: [N]Felt252 = undefined;
-    for (result) |r| {
-        r.* = Felt252.fromInt(num_copy & bitmask);
-        num_copy >>= num_bits_shift;
-    }
-    return result;
-}
-const std = @import("std");
-const Felt252 = @import("../math/fields/starknet.zig").Felt252;
-const Int = std.math.big.int.Managed;
 const testing = std.testing;
 
 pub fn split(allocator: std.mem.Allocator, num: Int, comptime N: usize, num_bits_shift: usize) ![N]Felt252 {
@@ -59,6 +37,7 @@ pub fn pack(allocator: std.mem.Allocator, comptime N: usize, limbs: [N]Felt252, 
     }
     return result;
 }
+
 test "split64 with uint utils" {
     var num = try Int.initSet(testing.allocator, 850981239023189021389081239089023);
     defer num.deinit();
