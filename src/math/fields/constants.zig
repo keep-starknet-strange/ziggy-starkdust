7b991860eff10b9ddf4e2da8d25a3ba69f554a6c
Sweta Shaw <swt.shw94@gmail.com>
2024-03-18 18:47:49 +0530

    update type

--- src/math/fields/constants.zig
+++ src/math/fields/constants.zig
@@ -4,4 +4,4 @@ const std = @import("std");
 pub const STARKNET_PRIME = 0x800000000000011000000000000000000000000000000000000000000000001;
 pub const FELT_BYTE_SIZE = 32;
 
-pub const BASE = std.math.pow(u128, 2, 86);
+pub const BASE = std.math.pow(u256, 2, 86);
