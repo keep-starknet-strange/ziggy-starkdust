4bc0eb3e9141636e8d07e7878dcc8c977387440d
Tuan Tran <tropicaldog17@gmail.com>
2024-03-28 20:29:38 +0700

    add tests for blake2s compress

--- src/hint_processor/blake2s_hash.zig
+++ src/hint_processor/blake2s_hash.zig
@@ -141,16 +141,82 @@ test "blake2s compress: test a" {
     try std.testing.expectEqualSlices(u32, &[8]u32{ 412110711, 3234706100, 3894970767, 982912411, 937789635, 742982576, 3942558313, 1407547065 }, new_state.items);
 }
 
-test "mix" {
-    const a = 1795745351;
-    const b = 1359893119;
-    const c = 1779033703;
-    const d = 1359893119;
-    const m0 = 0;
-    const m1 = 0;
-
-    const result = mix(a, b, c, d, m0, m1);
-    try testing.expectEqual(struct { u32, u32, u32, u32 }{ 3692008830, 2152139190, 1014794232, 1740027000 }, result);
+test "blake2s compress: test b" {
+    const h = [8]u32{
+        1795745351, 3144134277, 1013904242, 2773480762, 1359893119, 2600822924, 528734635,
+        1541459225,
+    };
+    const message = [_]u32{ 456710651, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    const new_state = try blake2s_compress(testing.allocator, h, message, 2, 0, 4294967295, 0);
+    defer new_state.deinit();
+    try std.testing.expectEqualSlices(u32, &[8]u32{ 1061041453, 3663967611, 2158760218, 836165556, 3696892209, 3887053585, 2675134684, 2201582556 }, new_state.items);
 }
 
-test "rotate" {}
+test "blake2s compress: test c" {
+    const h = [8]u32{
+        1795745351, 3144134277, 1013904242, 2773480762, 1359893119, 2600822924, 528734635,
+        1541459225,
+    };
+    const message = [_]u32{
+        1819043144, 1870078063, 6581362, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    };
+    const new_state = try blake2s_compress(testing.allocator, h, message, 9, 0, 4294967295, 0);
+    defer new_state.deinit();
+    try std.testing.expectEqualSlices(u32, &[8]u32{ 939893662, 3935214984, 1704819782, 3912812968, 4211807320, 3760278243, 674188535, 2642110762 }, new_state.items);
+}
+
+test "blake2s compress: test d" {
+    const h = [8]u32{
+        1795745351, 3144134277, 1013904242, 2773480762, 1359893119, 2600822924, 528734635,
+        1541459225,
+    };
+    const message = [_]u32{
+        1819043144, 1870078063, 6581362, 274628678, 715791845, 175498643, 871587583, 0, 0, 0,
+        0,          0,          0,       0,         0,         0,
+    };
+    const new_state = try blake2s_compress(testing.allocator, h, message, 28, 0, 4294967295, 0);
+    defer new_state.deinit();
+    try std.testing.expectEqualSlices(u32, &[8]u32{ 3980510537, 3982966407, 1593299263, 2666882356, 3288094120, 2682988286, 1666615862, 378086837 }, new_state.items);
+}
+
+test "blake2s compress: test e" {
+    const h = [8]u32{
+        1795745351, 3144134277, 1013904242, 2773480762, 1359893119, 2600822924, 528734635,
+        1541459225,
+    };
+    const message = [_]u32{
+        1819043144, 1870078063, 6581362,   274628678, 715791845, 175498643, 871587583, 635963558,
+        557369694,  1576875962, 215769785, 0,         0,         0,         0,         0,
+    };
+    const new_state = try blake2s_compress(testing.allocator, h, message, 44, 0, 4294967295, 0);
+    defer new_state.deinit();
+    try std.testing.expectEqualSlices(u32, &[8]u32{ 3251785223, 1946079609, 2665255093, 3508191500, 3630835628, 3067307230, 3623370123, 656151356 }, new_state.items);
+}
+
+test "blake2s compress: test f" {
+    const h = [8]u32{
+        1795745351, 3144134277, 1013904242, 2773480762, 1359893119, 2600822924, 528734635,
+        1541459225,
+    };
+    const message = [_]u32{
+        1819043144, 1870078063, 6581362,   274628678, 715791845, 175498643, 871587583, 635963558,
+        557369694,  1576875962, 215769785, 152379578, 585849303, 764739320, 437383930, 74833930,
+    };
+    const new_state = try blake2s_compress(testing.allocator, h, message, 64, 0, 4294967295, 0);
+    defer new_state.deinit();
+    try std.testing.expectEqualSlices(u32, &[8]u32{ 2593218707, 3238077801, 914875393, 3462286058, 4028447058, 3174734057, 2001070146, 3741410512 }, new_state.items);
+}
+
+test "blake2s compress: test g" {
+    const h = [8]u32{
+        1795745351, 3144134277, 1013904242, 2773480762, 1359893119, 2600822924, 528734635,
+        1541459225,
+    };
+    const message = [_]u32{
+        11563522,  43535528,   653255322, 274628678, 73471943,  17549868, 87158958,  635963558,
+        343656565, 1576875962, 215769785, 152379578, 585849303, 76473202, 437253230, 74833930,
+    };
+    const new_state = try blake2s_compress(testing.allocator, h, message, 64, 0, 4294967295, 0);
+    defer new_state.deinit();
+    try std.testing.expectEqualSlices(u32, &[8]u32{ 3496615692, 3252241979, 3771521549, 2125493093, 3240605752, 2885407061, 3962009872, 3845288240 }, new_state.items);
+}
